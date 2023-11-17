c1272a2789b6bf9a6eaea96ad2421ac16043f784
Terra <ichbinterra@gmail.com>
2023-11-17 19:42:30 +0100

    added treesitter for elixir

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "typescript", "tsx", "http", "json" })
+        vim.list_extend(opts.ensure_installed, { "typescript", "tsx", "http", "json", "elixir" })
       end
     end,
   },
