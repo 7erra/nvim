b8d9d1e7e570f306579199c4e41c47be269dcd03
Terra <ichbinterra@gmail.com>
2024-05-25 20:36:05 +0200

    Replace neo-tree with mini-files

---
+++ lua/plugins/mini-files.lua
@@ -0,0 +1,26 @@
+return {
+  "echasnovski/mini.files",
+  opts = {
+    opts = {
+      windows = {
+        preview = true,
+        width_focus = 30,
+        width_preview = 30,
+      },
+      options = {
+        -- Whether to use for editing directories
+        -- Disabled by default in LazyVim because neo-tree is used for that
+        use_as_default_explorer = true,
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>e",
+      function()
+        require("mini.files").open(vim.uv.cwd(), true)
+      end,
+      desc = "Open mini.files (cwd)",
+    },
+  },
+}
