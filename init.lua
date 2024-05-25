fdf19e21196689e22a7538d547f8a812588618dd
Terra <ichbinterra@gmail.com>
2024-05-25 20:07:19 +0200

    Removed deprecated snippet engine

--- init.lua
+++ init.lua
@@ -3,7 +3,3 @@ require("config.lazy")
 
 -- User commands
 local command = vim.api.nvim_create_user_command
-command("SnipsEdit", require("luasnip.loaders").edit_snippet_files, {})
-
--- Load snippets
-require("luasnip.loaders.from_snipmate").lazy_load()
