a912445c9ebabc125ebb704466c26332bc65627a
Terra <ichbinterra@gmail.com>
2024-06-18 09:58:33 +0200

    Customize lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,167 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = function()
+    -- PERF: we don't need this lualine require madness 🤷
+    local lualine_require = require("lualine_require")
+    lualine_require.require = require
+
+    local icons = LazyVim.config.icons
+
+    vim.o.laststatus = vim.g.lualine_laststatus
+
+    local opts = {
+      options = {
+        theme = "auto",
+        globalstatus = vim.o.laststatus == 3,
+        disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
+      },
+      sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {
+          { LazyVim.lualine.pretty_path() },
+        },
+        lualine_x = {
+          {
+            "diagnostics",
+            symbols = {
+              error = icons.diagnostics.Error,
+              warn = icons.diagnostics.Warn,
+              info = icons.diagnostics.Info,
+              hint = icons.diagnostics.Hint,
+            },
+          },
+          {
+            function()
+              return require("noice").api.status.command.get()
+            end,
+            cond = function()
+              return package.loaded["noice"] and require("noice").api.status.command.has()
+            end,
+            color = function()
+              return LazyVim.ui.fg("Statement")
+            end,
+          },
+          {
+            function()
+              return require("noice").api.status.mode.get()
+            end,
+            cond = function()
+              return package.loaded["noice"] and require("noice").api.status.mode.has()
+            end,
+            color = function()
+              return LazyVim.ui.fg("Constant")
+            end,
+          },
+          {
+            function()
+              return "  " .. require("dap").status()
+            end,
+            cond = function()
+              return package.loaded["dap"] and require("dap").status() ~= ""
+            end,
+            color = function()
+              return LazyVim.ui.fg("Debug")
+            end,
+          },
+          {
+            require("lazy.status").updates,
+            cond = require("lazy.status").has_updates,
+            color = function()
+              return LazyVim.ui.fg("Special")
+            end,
+          },
+          {
+            "diff",
+            symbols = {
+              added = icons.git.added,
+              modified = icons.git.modified,
+              removed = icons.git.removed,
+            },
+            source = function()
+              local gitsigns = vim.b.gitsigns_status_dict
+              if gitsigns then
+                return {
+                  added = gitsigns.added,
+                  modified = gitsigns.changed,
+                  removed = gitsigns.removed,
+                }
+              end
+            end,
+          },
+          {
+            -- https://github.com/nvim-lualine/lualine.nvim/discussions/1153
+            function()
+              -- Check if 'conform' is available
+              local status, conform = pcall(require, "conform")
+              if not status then
+                return "Conform not installed"
+              end
+
+              local lsp_format = require("conform.lsp_format")
+
+              -- Get formatters for the current buffer
+              local formatters = conform.list_formatters_for_buffer()
+              if formatters and #formatters > 0 then
+                local formatterNames = {}
+
+                for _, formatter in ipairs(formatters) do
+                  table.insert(formatterNames, formatter)
+                end
+
+                return "󰷈 " .. table.concat(formatterNames, " ")
+              end
+
+              -- Check if there's an LSP formatter
+              local bufnr = vim.api.nvim_get_current_buf()
+              local lsp_clients = lsp_format.get_format_clients({ bufnr = bufnr })
+
+              if not vim.tbl_isempty(lsp_clients) then
+                return "󰷈 LSP Formatter"
+              end
+
+              return ""
+            end,
+          },
+          {
+            -- LSP Info
+            function()
+              local buf_clients = vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf() })
+              if next(buf_clients) == nil then
+                return "󰒏 No LSP"
+              end
+              local client_names = {}
+              for _, client in pairs(buf_clients) do
+                table.insert(client_names, client.name)
+              end
+              return "󰒋 " .. table.concat(client_names, ", ")
+            end,
+          },
+        },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      extensions = {},
+    }
+
+    -- do not add trouble symbols if aerial is enabled
+    if vim.g.trouble_lualine then
+      local trouble = require("trouble")
+      local symbols = trouble.statusline
+        and trouble.statusline({
+          mode = "symbols",
+          groups = {},
+          title = false,
+          filter = { range = true },
+          format = "{kind_icon}{symbol.name:Normal}",
+          hl_group = "lualine_c_normal",
+        })
+      table.insert(opts.sections.lualine_c, {
+        symbols and symbols.get,
+        cond = symbols and symbols.has,
+      })
+    end
+
+    return opts
+  end,
+}
