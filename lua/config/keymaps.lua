5277a331553a0da0b4d5752e1146ef27e44ad788
Terra <ichbinterra@gmail.com>
2024-08-14 13:45:03 +0200

    Add copilot-chat extra

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,7 +5,7 @@ local wk = require("which-key")
 
 local map = vim.keymap.set
 
-map("n", "<leader>a", "gg0vG$", { desc = "Mark entire file", remap = true })
+map("n", "<leader>fa", "gg0vG$", { desc = "Mark entire file", remap = true })
 map("v", "<C-r>", '"hy:%s/<C-r>h//gc<left><left><left>', { desc = "Replace current word in file", remap = true })
 map("n", "<leader><Tab>p", function()
   vim.cmd("tabprevious")
@@ -19,9 +19,3 @@ map({ "n", "v" }, "<leader>rl", "<Plug>(RestNvimLast)", { desc = "Rerun the last
 -- wk.register({ ["<leader>r"] = { name = "+REST" } })
 wk.add({ "<leader>r", group = "REST" }, { "<leader>p", group = "+Copilot" })
 map("n", "<leader>m", vim.cmd.make, { desc = ":make" })
--- wk.register({ ["<leader>p"] = { name = "+Copilot" } })
--- wk.register({ ["<leader>p"] = { mode = "v", name = "+Copilot" } })
-map({ "n", "v" }, "<leader>pc", ":CopilotChat<Enter>", { desc = "Open Copilot Chat" })
-map({ "n", "v" }, "<leader>pe", ":CopilotChatExplain<Enter>", { desc = "Explain selection" })
-map({ "n", "v" }, "<leader>pf", ":CopilotChatFix<Enter>", { desc = "Fix" })
-map({ "n", "v" }, "<leader>pd", ":CopilotChatDocs<Enter>", { desc = "Document selection" })
