71517ab0d985b711c4e3da41f59005bef722b5ba
Terra <ichbinterra@gmail.com>
2025-03-06 13:07:35 +0100

    Catpuccin colorscheme with black background

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,22 @@
 return {
-  "Everblush/nvim",
-  name = "everblush",
-  config = function()
-    vim.cmd("colorscheme everblush")
-  end,
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
 }
