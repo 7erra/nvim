19bc16d2acd95e1e86c83d285ea7d52fcaa90238
Terra <ichbinterra@gmail.com>
2025-03-08 21:07:51 +0100

    Fix broken mini.files config

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -17,17 +17,15 @@ return {
         return true -- Show other files
       end,
     },
-    opts = {
-      windows = {
-        preview = true,
-        width_focus = 30,
-        width_preview = 30,
-      },
-      options = {
-        -- Whether to use for editing directories
-        -- Disabled by default in LazyVim because neo-tree is used for that
-        use_as_default_explorer = true,
-      },
+    windows = {
+      preview = true,
+      width_focus = 30,
+      width_preview = 30,
+    },
+    options = {
+      -- Whether to use for editing directories
+      -- Disabled by default in LazyVim because neo-tree is used for that
+      use_as_default_explorer = true,
     },
   },
   keys = {
